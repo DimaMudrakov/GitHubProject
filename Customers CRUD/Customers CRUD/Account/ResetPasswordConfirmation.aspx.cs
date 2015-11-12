b5934f5d3f930f5abe252408a8fa63d6421fa3de
Dima Mudrakov <d.mudrakov@gmail.com>
2015-11-12 23:37:11 +0200

    Create customers CRUD

---
+++ Customers CRUD/Customers CRUD/Account/ResetPasswordConfirmation.aspx.cs
@@ -0,0 +1,8 @@
+﻿using System.Web.UI;
+
+namespace Customers_CRUD.Account
+{
+    public partial class ResetPasswordConfirmation : Page
+    {
+    }
+}
---
+++ Customers CRUD/Customers CRUD/Startup.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(Customers_CRUD.Startup))]
+namespace Customers_CRUD
+{
+    public partial class Startup {
+        public void Configuration(IAppBuilder app) {
+            ConfigureAuth(app);
+        }
+    }
+}
