10f9a235759f1aa3517fab92f1df87324e0e0b4f
Dima Mudrakov <d.mudrakov@gmail.com>
2016-01-16 00:45:45 +0200

    Did parser, left add some styles.

--- SimpleParse/SimpleParse/Form1.Designer.cs
+++ SimpleParse/SimpleParse/Form1.Designer.cs
@@ -16,44 +16,64 @@
         private void InitializeComponent()
         {
             this.button1 = new System.Windows.Forms.Button();
-            this.tableLayoutPanel1 = new System.Windows.Forms.TableLayoutPanel();
+            this.tableEPL = new System.Windows.Forms.TableLayoutPanel();
             this.SuspendLayout();
             // 
             // button1
             // 
-            this.button1.Location = new System.Drawing.Point(536, 308);
+            this.button1.Location = new System.Drawing.Point(616, 405);
             this.button1.Name = "button1";
             this.button1.Size = new System.Drawing.Size(75, 23);
             this.button1.TabIndex = 0;
             this.button1.Text = "button1";
             this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
             // 
-            // tableLayoutPanel1
+            // tableEPL
             // 
-            this.tableLayoutPanel1.ColumnCount = 6;
-            this.tableLayoutPanel1.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Percent, 34.78261F));
-            this.tableLayoutPanel1.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Percent, 65.21739F));
-            this.tableLayoutPanel1.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Absolute, 104F));
-            this.tableLayoutPanel1.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Absolute, 108F));
-            this.tableLayoutPanel1.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Absolute, 89F));
-            this.tableLayoutPanel1.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Absolute, 54F));
-            this.tableLayoutPanel1.Location = new System.Drawing.Point(12, 12);
-            this.tableLayoutPanel1.Name = "tableLayoutPanel1";
-            this.tableLayoutPanel1.RowCount = 3;
-            this.tableLayoutPanel1.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Percent, 42F));
-            this.tableLayoutPanel1.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Percent, 58F));
-            this.tableLayoutPanel1.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Absolute, 20F));
-            this.tableLayoutPanel1.Size = new System.Drawing.Size(599, 223);
-            this.tableLayoutPanel1.TabIndex = 1;
-            this.tableLayoutPanel1.Paint += new System.Windows.Forms.PaintEventHandler(this.tableLayoutPanel1_Paint);
+            this.tableEPL.CellBorderStyle = System.Windows.Forms.TableLayoutPanelCellBorderStyle.Single;
+            this.tableEPL.ColumnCount = 6;
+            this.tableEPL.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Absolute, 60F));
+            this.tableEPL.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Absolute, 200F));
+            this.tableEPL.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Absolute, 60F));
+            this.tableEPL.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Absolute, 60F));
+            this.tableEPL.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Absolute, 60F));
+            this.tableEPL.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Absolute, 60F));
+            this.tableEPL.Location = new System.Drawing.Point(12, 12);
+            this.tableEPL.MinimumSize = new System.Drawing.Size(0, 5);
+            this.tableEPL.Name = "tableEPL";
+            this.tableEPL.RowCount = 21;
+            this.tableEPL.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Absolute, 20F));
+            this.tableEPL.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Absolute, 15F));
+            this.tableEPL.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Absolute, 15F));
+            this.tableEPL.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Absolute, 15F));
+            this.tableEPL.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Absolute, 15F));
+            this.tableEPL.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Absolute, 15F));
+            this.tableEPL.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Absolute, 15F));
+            this.tableEPL.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Absolute, 15F));
+            this.tableEPL.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Absolute, 15F));
+            this.tableEPL.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Absolute, 15F));
+            this.tableEPL.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Absolute, 15F));
+            this.tableEPL.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Absolute, 15F));
+            this.tableEPL.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Absolute, 15F));
+            this.tableEPL.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Absolute, 15F));
+            this.tableEPL.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Absolute, 15F));
+            this.tableEPL.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Absolute, 15F));
+            this.tableEPL.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Absolute, 15F));
+            this.tableEPL.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Absolute, 15F));
+            this.tableEPL.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Absolute, 15F));
+            this.tableEPL.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Absolute, 15F));
+            this.tableEPL.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Absolute, 15F));
+            this.tableEPL.Size = new System.Drawing.Size(611, 347);
+            this.tableEPL.TabIndex = 1;
             // 
             // Form1
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.BackColor = System.Drawing.SystemColors.ButtonHighlight;
-            this.ClientSize = new System.Drawing.Size(623, 343);
-            this.Controls.Add(this.tableLayoutPanel1);
+            this.ClientSize = new System.Drawing.Size(734, 494);
+            this.Controls.Add(this.tableEPL);
             this.Controls.Add(this.button1);
             this.Cursor = System.Windows.Forms.Cursors.Hand;
             this.Name = "Form1";
@@ -66,7 +86,7 @@
 
 
         private System.Windows.Forms.Button button1;
-        private System.Windows.Forms.TableLayoutPanel tableLayoutPanel1;
+        private System.Windows.Forms.TableLayoutPanel tableEPL;
     }
 }
 
--- SimpleParse/SimpleParse/Form1.cs
+++ SimpleParse/SimpleParse/Form1.cs
@@ -1,10 +1,11 @@
 ﻿using System;
-using System.Collections.Generic;
+using System.Collections.Generic;   
 using System.ComponentModel;
 using System.Data;
 using System.Drawing;
 using System.Linq;
 using System.Text;
+using System.Text.RegularExpressions;
 using System.Threading.Tasks;
 using System.Windows.Forms;
 using xNet;
@@ -20,8 +21,8 @@ namespace SimpleParse
 
         private void Form1_Load(object sender, EventArgs e)
         {
-            GetCaptionTable();
-            MessageBox.Show(GetSourcePage());
+            DisplayCaptionTable();
+            DisplayPositionNumbers();
         }
 
         private string GetSourcePage()
@@ -36,21 +37,69 @@ namespace SimpleParse
             return sourcePage;
         }
 
-        private void GetCaptionTable()
-        {
-            TableLayoutPanel table = tableLayoutPanel1;
-
-            table.Controls.Add(new Label() { Text = "№" }, 0, 0);
-            table.Controls.Add(new Label() { Text = "Команда" }, 1, 0);
-            table.Controls.Add(new Label() { Text = "И" }, 2, 0);
-            table.Controls.Add(new Label() { Text = "З" }, 3, 0);
-            table.Controls.Add(new Label() { Text = "П" }, 4, 0);
-            table.Controls.Add(new Label() { Text = "О" }, 5, 0);
+        private void DisplayCaptionTable()
+        { 
+            tableEPL.Controls.Add(new Label() { Text = "№" }, 0, 0);
+            tableEPL.Controls.Add(new Label() { Text = "Команда" }, 1, 0);
+            tableEPL.Controls.Add(new Label() { Text = "И" }, 2, 0);
+            tableEPL.Controls.Add(new Label() { Text = "З" }, 3, 0);
+            tableEPL.Controls.Add(new Label() { Text = "П" }, 4, 0);
+            tableEPL.Controls.Add(new Label() { Text = "О" }, 5, 0);
         }
 
-        private void tableLayoutPanel1_Paint(object sender, PaintEventArgs e)
+        private void DisplayPositionNumbers()
         {
+            for (int i = 1; i <= 20; i++)
+            {
+                string number = i.ToString();
+                tableEPL.Controls.Add(new Label() { Text = number }, 0, i);
+            }
+        }
 
+        private void DisplayTableWithReuslt(string sourcePage)
+        {
+            
+            string[] teams = sourcePage.Substrings("<td class=\"tt2 tt_title\"><a href=\"/stats/team/", "</a>", 0);
+
+            for (int i = 0; i < teams.Length; i++)
+            {
+                string regteam = teams[i];
+                var match = Regex.Match(regteam, "[^a-zA-Z0-9>]+$");
+                string team = match.Value;
+                tableEPL.Controls.Add(new Label { Text = team, MaximumSize = new Size(1500, 0), AutoSize = true }, 1, 1 + i);
+            }
+
+            string[] quantityMatches = sourcePage.Substrings("<td class=\"tt3\">", "</td>", 1);
+
+            for (int i = 1; i < quantityMatches.Length; i++)
+            {
+                tableEPL.Controls.Add(new Label() { Text = quantityMatches[i] }, 2, i);
+            }
+
+            string[] goalsScored = sourcePage.Substrings("<td class=\"tt7\">", "</td>", 1);
+
+            for (int i = 1; i < goalsScored.Length; i++)
+            {
+                tableEPL.Controls.Add(new Label() { Text = goalsScored[i] }, 3, i);
+            }
+
+            string[] goalsMissed = sourcePage.Substrings("<td class=\"tt8\">", "</td>", 1);
+
+            for (int i = 1; i < goalsMissed.Length; i++)
+            {
+                tableEPL.Controls.Add(new Label() { Text = goalsMissed[i] }, 4, i);
+            }
+            string[] points = sourcePage.Substrings("<td class=\"tt10\">", "</td>", 1);
+
+            for (int i = 1; i < points.Length; i++)
+            {
+                tableEPL.Controls.Add(new Label() { Text = points[i] }, 5, i);
+            }
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            DisplayTableWithReuslt(GetSourcePage());
         }
     }
 }
