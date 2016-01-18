81577865c175be2d9f8d2d7672b5a165fca23b2f
Dima Mudrakov <d.mudrakov@gmail.com>
2016-01-18 16:39:31 +0200

    First commit PluginLibrary

---
+++ PluginLibrary/PluginLibrary/Properties/AssemblyInfo.cs
@@ -0,0 +1,19 @@
+﻿using System.Reflection;
+using System.Runtime.InteropServices;
+using System.Windows;
+
+[assembly: AssemblyTitle("PluginLibrary")]
+[assembly: AssemblyDescription("Plugins Library")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("PluginLibrary")]
+[assembly: AssemblyCopyright("Copyright ©  2016")]
+[assembly: AssemblyTrademark("MDV")]
+[assembly: AssemblyCulture("")]
+[assembly: ComVisible(false)]
+[assembly: ThemeInfo(
+    ResourceDictionaryLocation.None, 
+    ResourceDictionaryLocation.SourceAssembly
+)]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
